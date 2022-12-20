0c73fdced59569725886fccc143509e325e4becc
Rohit Kumar <61248027+Lycanwave@users.noreply.github.com>
2022-12-20 20:08:53 +0530

    initialization and properties

---
+++ stack_initialization.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+int main(){
+
+    //stack initialization
+    //stack<data_type>
+    stack<int> st;
+
+    st.size();//Number of element in stack.
+
+    st.push();//inserting the element inside stack
+
+    st.pop();//popping the element from stack
+
+    st.empty();//return true if stack is empty else return false
+
+    st.top();//return the top element of stack
+
+
+    return 0;
+}
